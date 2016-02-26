d84742d43c1cc220731dacd44a346f149f9e6254
SourceOrbit <main@sourceorbitgaming.com>
2016-02-26 22:56:50 +1000

    second commit

---
+++ ProjectSettings/Assets/Scripts/MoveLeftAndRight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+		//This moves our object left or right based on input
+		transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+	}
+}
