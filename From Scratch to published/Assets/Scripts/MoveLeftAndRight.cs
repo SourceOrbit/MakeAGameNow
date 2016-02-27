1b977d5c4c98a3807b338e5f733f15b806e733c1
SourceOrbit <main@sourceorbitgaming.com>
2016-02-27 17:02:42 +1000

    New Unity project, Enemy Explode

---
+++ From Scratch to published/Assets/Scripts/MoveLeftAndRight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;	}
+ 	}
